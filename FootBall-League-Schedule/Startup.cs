cf2f9634b99d220faa132a2a202b8afbc6619dd1
Lê Đặng Hoài Nam <namhoai95@gmail.com>
2018-03-08 12:15:02 +0700

    Add CORS

--- FootBall-League-Schedule/Startup.cs
+++ FootBall-League-Schedule/Startup.cs
@@ -21,6 +21,7 @@ using Business.Services;
 using FootBallLeagueSchedule.Helpers;
 using Newtonsoft.Json.Serialization;
 using Business.implement_fake_data;
+using System.Threading.Tasks;
 
 namespace FootBallLeagueSchedule
 {
@@ -113,6 +114,7 @@ namespace FootBallLeagueSchedule
             }
             //app.UseResponseCaching();
             //app.UseHttpCacheHeaders();
+            app.UseCorsMiddleware();
             app.UseMvc();
             app.UseSwagger();
             app.UseSwaggerUI(c =>
@@ -138,5 +140,33 @@ namespace FootBallLeagueSchedule
             });
         }
 
+    }
+
+    public class CorsMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CorsMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            httpContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            httpContext.Response.Headers.Add("Access-Control-Allow-Credentials", "true");
+            httpContext.Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version, X-File-Name");
+            httpContext.Response.Headers.Add("Access-Control-Allow-Methods", "POST,GET,PUT,PATCH,DELETE,OPTIONS");
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class CorsMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCorsMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CorsMiddleware>();
+        }
     }
 }
